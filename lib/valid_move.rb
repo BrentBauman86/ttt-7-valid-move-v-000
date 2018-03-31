8b38ba38a0ca627f46c7f4e561be7ee1c0ea570e
Brent Bauman <brentbauman86@gmail.com>
2018-03-31 16:34:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
-  if position_taken? && .between?(0, 8)
+  if position_taken? && board[index].between?(0, 8)
     true
-  else board[index] ==
+  else board[index] == 
     false
 end
 end
@@ -13,3 +13,5 @@ if board[index] == "" || board[index] == " " || board[index] == nil
       true
 end
 end
+
+
